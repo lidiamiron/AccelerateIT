4ada788daf528014c05d46c95ac2a77b2c9d6d0b
Christian D. Dunca <christian.dunca@gmail.com>
2024-02-08 14:10:20 +0200

    add EmailJS service

--- index.js
+++ index.js
@@ -3,62 +3,57 @@ const inputList = document.getElementsByTagName('input');
 
 //phone country code
 const phoneInputField = document.querySelector("#phone");
-   const phoneInput = window.intlTelInput(phoneInputField, {
-    initialCountry: "ro",
-    preferredCountries: ["ro", "gb", "de", "fr"],
-    utilsScript:
-      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+const phoneInput = window.intlTelInput(phoneInputField, {
+  initialCountry: "ro",
+  preferredCountries: ["ro", "gb", "de", "fr"],
+  utilsScript:
+  "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
   });
 
 //copy-right year auto-update
 const currentYear =  new Date().getFullYear();
 document.querySelector('#copyright-year').innerHTML = currentYear;
 
-function test(){
+function calculateAge(dob) { 
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms); 
+
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+
+// EmailJS service
+function sendMail() {
   const params = {
-    firstName: document.querySelector('#firstName').value,
-    lastName: document.querySelector('#lastName').value,
-    email: document.querySelector('#email').value,
-    phone: document.querySelector('#phone').value,
-    courseType: document.querySelector('#course-type').value,
-    dob: document.querySelector('#dob').value,
-    message: document.querySelector('#message').value
+  firstName: document.querySelector('#firstName')?.value,
+  lastName: document.querySelector('#lastName')?.value,
+  email: document.querySelector('#email')?.value,
+  countryCode: document.querySelector('.iti__selected-flag')?.title,
+  phone: document.querySelector('#phone')?.value,
+  courseType: document.querySelector('#course-type')?.value,
+  dob: document.querySelector('#dob')?.value,
+  age: calculateAge(new Date(document.querySelector('#dob')?.value,)),
+  message: document.querySelector('#message')?.value,
+  gdpr: document.querySelector('#gdpr')?.checked ? 'DA' : 'NU'
   }
-  console.log(params);
-} 
 
-//EmailJS service
-// function initializeEmailJS() {
-//   emailjs.init("2AqA444fRgyiaZXV_");
-// }
-// initializeEmailJS();
+  const serviceId = 'service_uu7ki1n';
+  const templateId = 'template_se5geg2';
 
-// function sendMail() {
-//   const params = {
-//     firstName: document.querySelector('#firstName').value,
-//     lastName: document.querySelector('#lastName').value,
-//     email: document.querySelector('#email').value,
-//     phone: document.querySelector('#phone').value,
-//     courseType: document.querySelector('#course-type').value,
-//     dob: document.querySelector('#dob').value,
-//     message: document.querySelector('#message').value
-//   }
-
-//   const serviceId = 'service_uu7ki1n';
-//   const templateId = 'template_se5geg2';
-
-//   emailjs.send(serviceId, templateId, params)
-//   .then( res => {
-//     document.querySelector('#firstName').value = '';
-//     document.querySelector('#lastName').value = '';
-//     document.querySelector('#email').value = '';
-//     document.querySelector('#phone').value = null;
-//     document.querySelector('#course-type').value  = null;
-//     document.querySelector('#dob').value = null;
-//     document.querySelector('#message').value = '';
-//   })
-//   .catch (error => console.log(error));
-// }
+  emailjs.send(serviceId, templateId, params)
+  .then( res => {
+    document.querySelector('#firstName').value = '';
+    document.querySelector('#lastName').value = '';
+    document.querySelector('#email').value = '';
+    document.querySelector('#phone').value = null;
+    document.querySelector('#course-type').value  = null;
+    document.querySelector('#dob').value = null;
+    document.querySelector('#message').value = '';
+    document.querySelector('#gdpr').checked = false;
+    console.log(res);
+  })
+  .catch (error => console.log(error));
+}
 
 
 
